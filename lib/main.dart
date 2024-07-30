2b39af7a0e9c9be5476cfa25685aa890d05e3b8d
Szymon Śliwiński <122596933+ssliwinski07@users.noreply.github.com>
2024-07-30 06:40:30 +0200

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -57,10 +57,6 @@ void main() async {
   //userId: 4, userName: Michal, lastName: Bonk, streetName: lesna 19, postalCode: 41-943, cityName: butom)
   //DSA
 
-  final result5 = await databaseServiceBase.getUsersGroups();
-
-  final result6 = await databaseServiceBase.getUsers();
-
   //final result2 = await databaseServiceBase.getUsersGroups();
 
   //print(result2);
@@ -183,149 +179,95 @@ void main() async {
   // await databaseServiceBase.insertToDatabase(
   //     json: userGroupToJson3, tableName: usersGroupsTable);
 
-  runApp(MyApp(
-    items: result5,
-    items2: result6,
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key, required this.items, required this.items2});
+  const MyApp({super.key});
 
-  List<GroupModel> items;
-  List<UserModel> items2;
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyHomePage(
-        title: 'BE',
-        items: items,
-        items2: items2,
-      ),
+      home: MainScreenView(title: "Users and groups"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage(
-      {super.key,
-      required this.title,
-      required this.items,
-      required this.items2});
-
-  List<GroupModel> items;
-  List<UserModel> items2;
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MainScreenView extends StatefulWidget {
+  MainScreenView({
+    super.key,
+    required this.title,
+  });
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainScreenView> createState() => _MainScreenViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainScreenViewState extends State<MainScreenView> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Colors.amberAccent,
         title: Text(widget.title),
       ),
       body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
+        padding: const EdgeInsets.all(15),
         crossAxisCount: 2,
+        mainAxisSpacing: 15,
+        crossAxisSpacing: 15,
         children: <Widget>[
           Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[100],
-            child: ListView.builder(
-                itemCount: widget.items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  var items = widget.items[index];
-                  var group = items.groupName;
-                  return Text(group);
-                }),
+            padding: const EdgeInsets.all(10),
+            color: Colors.teal,
+            child: Column(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.person,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(context.localize.users)
+              ],
+            ),
           ),
           Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[200],
-            child: ListView.builder(
-                itemCount: widget.items2.length,
-                itemBuilder: (BuildContext context, int index) {
-                  var items2 = widget.items2[index];
-                  var group = items2.userName;
-                  return Text(group);
-                }),
-          ),
+            padding: const EdgeInsets.all(10),
+            color: Colors.teal,
+            child: Column(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.people,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(context.localize.usersGroups)
+              ],
+            ),
+          )
         ],
       )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
